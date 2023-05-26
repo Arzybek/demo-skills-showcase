09c27949be80470c528017347cce9d256d58c39d
Arzybek <arzybek.kazikulov@gmail.com>
2023-05-27 04:52:36 +0500

    MASTER Init

---
+++ src/main/java/com/demo/showcase/ShowcaseApplication.java
@@ -0,0 +1,13 @@
+package com.demo.showcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowcaseApplication.class, args);
+	}
+
+}
