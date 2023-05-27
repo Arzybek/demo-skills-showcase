8a35fe5acabc9773d003edd5e4f6ac5874890d47
Arzybek <arzybek.kazikulov@gmail.com>
2023-05-27 19:25:44 +0500

    MASTER Init data file + swagger

--- common/enums/src/main/java/com/demo/showcase/common/enums/ShowGenre.java
+++ common/enums/src/main/java/com/demo/showcase/common/enums/ShowGenre.java
@@ -6,5 +6,7 @@ public enum ShowGenre {
     ACTION,
     THRILLER,
     SITCOM,
-    HORROR
+    HORROR,
+    ANIME,
+    CARTOON
 }
--- services/shows-data-service/src/main/java/com/demo/showcase/sds/api/ShowsDataApi.java
+++ services/shows-data-service/src/main/java/com/demo/showcase/sds/api/ShowsDataApi.java
@@ -1,6 +1,7 @@
 package com.demo.showcase.sds.api;
 
 import com.demo.showcase.common.data.ShowsEntity;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public interface ShowsDataApi {
     String SHOWS_DATA_API_PATH = "/shows";
 
     @GetMapping
+    @Operation(summary = "Получение списка всех зарегистрированных сериалов")
     List<ShowsEntity> getAll();
 
 //    @PostMapping
