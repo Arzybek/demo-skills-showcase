14a30bb8b489ee79f4aa064b64ac13b3a307757b
Arzybek <arzybek.kazikulov@gmail.com>
2023-05-27 05:14:29 +0500

    MASTER Structure

--- services/shows-data-service/src/main/java/com/demo/showcase/sds/ShowcaseApplication.java
+++ services/shows-data-service/src/main/java/com/demo/showcase/sds/ShowcaseApplication.java
@@ -1,4 +1,4 @@
-package com.demo.showcase;
+package com.demo.showcase.sds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/shows-data-service/src/test/java/com/demo/showcase/sds/ShowcaseApplicationTests.java
+++ services/shows-data-service/src/test/java/com/demo/showcase/sds/ShowcaseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.demo.showcase;
+package com.demo.showcase.sds;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
