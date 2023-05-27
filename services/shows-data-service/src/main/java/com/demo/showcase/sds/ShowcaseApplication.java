0dfb7e9da4381f4615bf05586464c39d7d03f6d9
Arzybek <arzybek.kazikulov@gmail.com>
2023-05-27 06:05:36 +0500

    MASTER Migrations service

--- services/shows-data-service/src/main/java/com/demo/showcase/sds/ShowcaseApplication.java
+++ services/shows-data-service/src/main/java/com/demo/showcase/sds/ShowcaseApplication.java
@@ -1,9 +1,11 @@
 package com.demo.showcase.sds;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@Slf4j
+@SpringBootApplication(scanBasePackages = "com.demo.showcase")
 public class ShowcaseApplication {
 
 	public static void main(String[] args) {
---
+++ services/shows-migrations-service/src/main/java/com/demo/showcase/migrations/ShowcaseMigrationsApp.java
@@ -0,0 +1,15 @@
+package com.demo.showcase.migrations;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ShowcaseMigrationsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowcaseMigrationsApp.class, args);
+    }
+
+}
